82e64876d9c6e653eedc1d45cbfc245daad617fd
Praveenkhan <85428959+Praveenkhan@users.noreply.github.com>
2021-06-12 15:39:22 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
     class Simple{  
         public static void main(String args[]){  
          System.out.println(Hello Java);
-            System.out.println(Hello Java 2);
-            System.out.println(Hello Java 3);
+            
         }  
     }  
